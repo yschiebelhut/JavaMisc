2cb0be02560da5274908ec0194e4f068aecb9272
JPHelios <43265991+JPHelios@users.noreply.github.com>
2021-06-29 13:47:53 +0200

    Ähm... ja...irgendwelche Änderungen, die ich vor Wochen mal gemacht habe

--- InputHelper.java
+++ InputHelper.java
@@ -1,5 +1,3 @@
-package de.schiebelhut.misc;
-
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -9,20 +7,22 @@ import java.util.Scanner;
  */
 public class InputHelper {
 
-
-	public static double getDoubleInput() {
+	public static double getDoubleInput(String output) {
 		while (true) {
 			try {
 				Scanner sc = new Scanner(System.in);
+				System.out.println(output);
 				return sc.nextDouble();
 			} catch (InputMismatchException e) {
 				System.out.println("Please enter a valid value of type double.");
 			} catch (Exception a) {
 				System.out.println("different error");
 			}
+
 		}
 	}
 
+	//This Integer input method just checks if the input is a valid integer
 	public static int getIntegerInput() {
 		while (true) {
 			try {
@@ -30,12 +30,51 @@ public class InputHelper {
 				return sc.nextInt();
 			} catch (InputMismatchException e) {
 				System.out.println("Please enter a valid value of type int.");
+			} catch (Exception a) {
+				System.out.println("different error");
 			}
 		}
 	}
 
-	public static String getStringInput() {
+	//This Integer input method just checks if the input is a valid integer and provides a descriptive string
+	public static int getIntegerInput(String output) {
+		while (true) {
+			try {
+				Scanner sc = new Scanner(System.in);
+				System.out.println(output);
+				return sc.nextInt();
+			} catch (InputMismatchException e) {
+				System.out.println("Please enter a valid value of type int.");
+			} catch (Exception a) {
+				System.out.println("different error");
+			}
+		}
+	}
+
+	//This Integer input method just checks if the input is in a special range of integer
+	public static int getIntegerInput(String output, int lowerBorder, int upperBorder) {
+		int input = 0;
+
+		do {
+			try {
+				Scanner sc = new Scanner(System.in);
+
+
+				System.out.println(output);
+				input = sc.nextInt();
+			} catch (InputMismatchException e) {
+				System.out.println("Please enter a valid value of type int.");
+			} catch (Exception a) {
+				System.out.println("different error");
+			}
+		} while (input < lowerBorder | input > upperBorder);
+
+		return input;
+	}
+
+	public static String getStringInput(String output) {
 		Scanner sc = new Scanner(System.in);
+		System.out.println(output);
 		return sc.next();
 	}
 
